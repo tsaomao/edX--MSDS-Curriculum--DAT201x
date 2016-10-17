d87161eef2bbece1f88e0b31274a52156f7ed742
Malcolm Gin <malcolm.gin@gmail.com>
2016-10-17 10:53:55 -0700

    Init Module 5

---
+++ Labs_Module5.sql
@@ -0,0 +1,13 @@
+/* EdX.org - DAT201x - Module 5 - Lab Queries */
+/* Environment setup document: https://github.com/MicrosoftLearning/QueryingT-SQL/blob/master/Getting%20Started%20with%20DAT201x.pdf */
+/* Lab Exercises:  */
+
+
+/* ----------------------------------------- */
+/* Challenge 1:   */
+/* 1.  */
+
+
+
+
+
---
+++ Lectures_Module5.sql
@@ -0,0 +1,8 @@
+/* EdX.org - DAT201x - Module 5 - Lecture Queries */
+
+/* Demo:  Using Functions and Aggregating Data */
+
+
+
+
+
