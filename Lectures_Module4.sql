ea0b1331fe747ac46ed610d1ef73f6a270d05c8b
Malcolm Gin <malcolm.gin@gmail.com>
2016-10-17 09:09:16 -0700

    Incremental

---
+++ Lectures_Module4.sql
@@ -0,0 +1,6 @@
+/* EdX.org - DAT201x - Module 4 - Lecture Queries */
+
+/* Demo:  Using (Logical/Mathematical) Set Operators */
+-- Based on but not the same as the example in the Lecture
+SELECT FirstName, LastName
+FROM SalesLT.Customer
