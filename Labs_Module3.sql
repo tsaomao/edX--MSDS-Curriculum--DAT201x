94e88f489c8894eb29a9fe329fd5ab48b0909498
Malcolm Gin <malcolm.gin@gmail.com>
2016-10-17 07:36:07 -0700

    Module 3 Lecture complete, prep for Module 3 Lab

---
+++ Labs_Module3.sql
@@ -0,0 +1,8 @@
+/* EdX.org - DAT201x - Module 3 - Lab Queries */
+/* Environment setup document: https://github.com/MicrosoftLearning/QueryingT-SQL/blob/master/Getting%20Started%20with%20DAT201x.pdf */
+/* Lab Exercises:  */
+
+
+/* ----------------------------------------------------- */
+/* Challenge 1:  */
+/* 1.  */
--- Lectures_Module3.sql
+++ Lectures_Module3.sql
@@ -1,2 +1,104 @@
 /* EdX.org - DAT201x - Module 3 - Lecture Queries */
 
+/* Demo: Using inner JOINs */
+-- Basic Inner Join
+SELECT SalesLT.Product.Name AS ProductName, SalesLT.ProductCategory.Name AS ProductCategory
+FROM SalesLT.Product
+INNER JOIN SalesLT.ProductCategory
+ON SalesLT.Product.ProductCategoryID = SalesLT.ProductCategory.ProductCategoryID;
+
+-- Table Aliases
+SELECT p.Name AS ProductName, c.Name AS Category
+FROM SalesLT.Product AS p
+JOIN SalesLT.ProductCategory as c
+ON p.ProductCategoryID = c.ProductCategoryID;
+
+-- Joining more than 2 tables
+SELECT oh.OrderDate, oh.SalesOrderNumber, p.Name AS ProductName, od.OrderQty, od.UnitPrice, od.LineTotal
+FROM SalesLT.SalesOrderHeader AS oh
+JOIN SalesLT.SalesOrderDetail AS od
+ON od.SalesOrderID = oh. SalesOrderID
+JOIN SalesLT.Product AS p
+ON od.ProductID = p.ProductID
+ORDER BY oh.OrderDate, oh.SalesOrderID, od.SalesOrderDetailID;
+
+-- Multiple JOIN Predicates
+SELECT oh.OrderDate, oh.SalesOrderNumber, p.Name AS ProductName, od.OrderQty, od.UnitPrice, od.LineTotal
+FROM SalesLT.SalesOrderHeader AS oh
+JOIN SalesLT.SalesOrderDetail AS od
+ON od.SalesOrderID = oh. SalesOrderID
+JOIN SalesLT.Product AS p
+ON od.ProductID = p.ProductID AND od.UnitPrice < p.ListPrice -- Multiple Predicates
+ORDER BY oh.OrderDate, oh.SalesOrderID, od.SalesOrderDetailID;
+
+/* Demo: Outer Joins */
+-- Get all customers, with sales orders for those who've bought anything
+SELECT c.FirstName, c.LastName, oh.SalesOrderNumber
+FROM SalesLT.Customer AS c
+LEFT OUTER JOIN SalesLT.SalesOrderHeader AS oh
+ON c.CustomerID = oh.CustomerID
+ORDER BY c.CustomerID;
+
+-- Return only customers who haven't purchased anything
+SELECT c.FirstName, c.LastName, oh.SalesOrderNumber
+FROM SalesLT.Customer AS c
+LEFT OUTER JOIN SalesLT.SalesOrderHeader AS oh
+ON c.CustomerID = oh.CustomerID
+WHERE oh.SalesOrderNumber IS NULL
+ORDER BY c.CustomerID;
+
+-- More than 2 tables
+SELECT p.Name AS ProductName, oh.SalesOrderNumber
+FROM SalesLT.Product AS p
+LEFT JOIN SalesLT.SalesOrderDetail AS od
+ON p.ProductID = od.ProductID
+LEFT JOIN SalesLT.SalesOrderHeader AS oh -- Additional tables also use LEFT JOIN to avoid screwing up criteria
+ON od.SalesOrderID = oh.SalesOrderID
+ORDER BY p.ProductID
+
+-- INNER JOIN within LEFT JOIN
+SELECT p.Name AS ProductName, c.Name AS Category, oh.SalesOrderNumber
+FROM SalesLT.Product AS p
+LEFT OUTER JOIN SalesLT.SalesOrderDetail AS od
+ON p.ProductID = od.ProductID
+LEFT OUTER JOIN SalesLT.SalesOrderHeader AS oh
+ON od.SalesOrderID = oh.SalesOrderID
+INNER JOIN SalesLT.ProductCategory AS c -- This table joined to the left table, so can use INNER JOIN
+ON p.ProductCategoryID = c.ProductCategoryID
+ORDER BY p.ProductID
+
+
+/* Demo: Cross JOINs and Cartesian Products */
+-- Basic Cross JOIN
+-- Returns about 250,000 rows
+SELECT p.Name, c.FirstName, c.LastName, c.Phone
+FROM SalesLT.Product as p
+CROSS JOIN SalesLT.Customer as c
+
+/* Demo: Self JOINs */
+-- Demo Prep:
+-- Be sure you have the right dB selected
+-- There's no Employee Table, so create one for this demo
+CREATE TABLE SalesLT.Employee
+(EmployeeID int IDENTITY PRIMARY KEY,
+EmployeeName nvarchar(256),
+ManagerID int);
+GO
+-- Get Salesperson from Customer table and generate managers
+INSERT INTO SalesLT.Employee (EmployeeName, ManagerID)
+Select DISTINCT Salesperson, NULLIF(CAST(RIGHT(SalesPerson, 1) as INT), 0)
+FROM SalesLT.Customer;
+GO
+UPDATE SalesLT.Employee 
+SET ManagerID = (SELECT MIN(EmployeeID) FROM SalesLT.Employee WHERE ManagerID IS NULL)
+WHERE ManagerID IS NULL
+AND EmployeeID > (SELECT MIN(EmployeeID) FROM SalesLT.Employee WHERE ManagerID IS NULL);
+GO
+
+-- Self JOIN demo
+SELECT e.EmployeeName, m.EmployeeName AS Manager
+FROM SalesLT.Employee AS e
+LEFT JOIN SalesLT.Employee AS m
+ON e.ManagerID = m.EmployeeID
+ORDER BY e.ManagerID;
+
