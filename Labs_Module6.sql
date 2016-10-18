4285f4ad19b8075081a5c0e5d648491a8f4e10db
Malcolm Gin <malcolm.gin@gmail.com>
2016-10-18 11:13:34 -0700

    Module 6, in progress.

--- Labs_Module6.sql
+++ Labs_Module6.sql
@@ -1,8 +1,62 @@
 /* EdX.org - DAT201x - Module 6 - Lab Queries */
 /* Environment setup document: https://github.com/MicrosoftLearning/QueryingT-SQL/blob/master/Getting%20Started%20with%20DAT201x.pdf */
-/* Lab Exercises:  */
+/* Lab Exercises: https://github.com/MicrosoftLearning/QueryingT-SQL/raw/master/Labs/Lab06.pdf */
 
 
-/* ----------------------------------------- */
-/* Challenge 1:  */
-/*  */
+/* ----------------------------------------------- */
+/* Challenge 1: Retrieve Product Price Information */
+/* 1. Retrieve products whose list price is higher than the average unit price
+Retrieve the product ID, name, and list price for each product where the list price is higher than the
+average unit price for all products that have been sold. */
+SELECT p.ProductID, p.Name, p.ListPrice
+FROM SalesLT.Product AS p
+WHERE p.ListPrice > 
+   (
+	SELECT MAX(od.UnitPrice)
+	FROM SalesLT.SalesOrderDetail AS od
+   );
+
+
+/* 2. Retrieve Products with a list price of $100 or more that have been sold for less than
+$100
+Retrieve the product ID, name, and list price for each product where the list price is $100 or more, and
+the product has been sold for less than $100. */
+SELECT p.ProductID, p.Name, p.ListPrice
+FROM SalesLT.Product AS p
+WHERE p.ListPrice > 100.00 AND p.ProductID IN 
+	(SELECT DISTINCT od.ProductID FROM SalesLT.SalesOrderDetail AS od WHERE od.UnitPrice < 100.00);
+
+
+/* 3. Retrieve the cost, list price, and average selling price for each product
+Retrieve the product ID, name, cost, and list price for each product along with the average unit price for
+which that product has been sold. */
+SELECT p.ProductID, p.Name, p.ListPrice,
+	(SELECT ISNULL(AVG(od.UnitPrice), 0.00) FROM SalesLT.SalesOrderDetail AS od WHERE od.ProductID = p.ProductID) AS AverageSalePrice
+FROM SalesLT.Product AS p
+ORDER BY p.ProductID;
+
+
+/* 4. Retrieve products that have an average selling price that is lower than the cost
+Filter your previous query to include only products where the cost price is higher than the average 
+selling price. */
+SELECT p.ProductID, p.Name, p.ListPrice,
+	(SELECT ISNULL(AVG(od.UnitPrice), 0.00) FROM SalesLT.SalesOrderDetail AS od WHERE od.ProductID = p.ProductID) AS AverageSalePrice
+FROM SalesLT.Product AS p
+WHERE p.ListPrice > (SELECT ISNULL(AVG(od.UnitPrice), 0.00) FROM SalesLT.SalesOrderDetail AS od WHERE od.ProductID = p.ProductID)
+ORDER BY p.ProductID;
+
+
+/* Challenge 2: Retrieve Customer Information */
+/* 1. Retrieve customer information for all sales orders
+Retrieve the sales order ID, customer ID, first name, last name, and total due for all sales orders from
+the SalesLT.SalesOrderHeader table and the dbo.ufnGetCustomerInformation function. */
+SELECT oh.SalesOrderID, oh.CustomerID, 
+	(SELECT c1.FirstName FROM dbo.ufnGetCustomerInformation(oh.CustomerID) AS c1) AS FirstName, 
+	(SELECT c2.LastName FROM dbo.ufnGetCustomerInformation(oh.CustomerID) AS c2) AS LastName, oh.TotalDue
+FROM SalesLT.SalesOrderHeader as oh;
+
+
+/* 2. Retrieve customer address information
+Retrieve the customer ID, first name, last name, address line 1 and city for all customers from the
+SalesLT.Address and SalesLT.CustomerAddress tables, and the dbo.ufnGetCustomerInformation
+function. */
--- Lectures_Module6.sql
+++ Lectures_Module6.sql
@@ -1,3 +1,68 @@
 /* EdX.org - DAT201x - Module 6 - Lecture Queries */
 
-/* Demo:   */
+/* Special, notable notes */
+/* - For TEXT literals, prepending the opening ' with N indicates a Unicode TEXT literal.
+     e.g. N'Mexico' is 'Mexico', but in Unicode. */
+
+/* Demo: Subqueries  */
+-- Display list of sales detail lines where ListPrice is greater than Product UnitPrice from sale.
+-- Inner query:
+SELECT MAX(UnitPrice) FROM SalesLT.SalesOrderDetail;
+
+-- Outer query:
+-- Note: Uses numeric literal from Inner query above
+-- For better usability, put Inner query into WHERE clause
+SELECT * FROM SalesLT.Product
+WHERE ListPrice > 1466.01;
+
+-- Complete query and subquery:
+SELECT * FROM SalesLT.Product
+WHERE ListPrice > 
+	(SELECT MAX(UnitPrice) FROM SalesLT.SalesOrderDetail);
+
+/* Demo: Correlated Subqueries */
+-- Note: changed slightly from lecture demo to use correct Table names
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM SalesLT.SalesOrderHeader AS SO1
+ORDER BY CustomerID, OrderDate;
+
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM SalesLT.SalesOrderHeader AS SO1
+WHERE OrderDate = 
+	(SELECT MAX(OrderDate) 
+	 FROM SalesLT.SalesOrderHeader);
+
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM SalesLT.SalesOrderHeader AS SO1
+WHERE OrderDate = 
+	(SELECT MAX(OrderDate) 
+	 FROM SalesLT.SalesOrderHeader AS SO2
+	 WHERE SO2.CustomerID = SO1.CustomerID)
+ORDER BY CustomerID;
+
+/* Demo: Using APPLY */
+-- Display Sales Order Details for items that are equal to 
+-- the maximum unit price for that sales order
+-- NOTE: Sample dB does not include udfMaxUnitPrice()
+SELECT oh.SalesOrderID, mup.MaximumUnitPrice
+FROM SalesLT.SalesOrderHeader AS oh
+CROSS APPLY SalesLT.udfMaxUnitPrice(oh.SalesOrderID) AS mup
+ORDER BY oh.SalesOrderID;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
