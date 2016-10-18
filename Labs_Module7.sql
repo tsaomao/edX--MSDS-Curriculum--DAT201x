2519613071f20c06fb9cffc0fd388024cc28c748
Malcolm Gin <malcolm.gin@gmail.com>
2016-10-18 14:15:30 -0700

    Module 7 Init.

---
+++ Labs_Module7.sql
@@ -0,0 +1,7 @@
+/* EdX.org - DAT201x - Module 6 - Lab Queries */
+/* Environment setup document: https://github.com/MicrosoftLearning/QueryingT-SQL/blob/master/Getting%20Started%20with%20DAT201x.pdf */
+/* Lab Exercises: https://github.com/MicrosoftLearning/QueryingT-SQL/raw/master/Labs/Lab06.pdf */
+
+
+/* ----------------------------------------------- */
+/* Challenge 1: Retrieve Product Price Information */
---
+++ Lectures_Module7.sql
@@ -0,0 +1,7 @@
+/* EdX.org - DAT201x - Module 7 - Lecture Queries */
+
+/* Special, notable notes */
+/* - For TEXT literals, prepending the opening ' with N indicates a Unicode TEXT literal.
+     e.g. N'Mexico' is 'Mexico', but in Unicode. */
+
+/* Demo: Subqueries  */
